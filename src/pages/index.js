525b79da5fddb5f70148773012367b0ddc209d8d
Prashantbag <prashant.bag14@gmail.com>
2022-09-08 08:53:21 +0530

    added blog cmd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Container from '../components/container';
 import HomeBanner from '../components/homeBanner';
-
+import { Link } from '@mui/material';
 
 export default function Index() {
     return (
         <Container>
            <HomeBanner/>
+           <Link href="/posts">Link</Link>
         </Container>
     );
 }
